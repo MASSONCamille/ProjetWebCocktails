007a43e70cde2d4c9274ab15a1b8529e41f31e1d
Lorian <lorian.sizaire@gmail.com>
2020-12-01 18:18:33 +0100

    Recherche

---
+++ Projet/script/get_tags_recherche.php
@@ -0,0 +1,14 @@
+<?php
+    include '../Donnees.inc.php';
+
+    $DebutRecherche = strtolower(trim($_GET['DebutRecherche']));
+
+    $Ingredients = array();
+    foreach($Hierarchie as $NomIngredient => $DetailsIngredient) {
+        if(strpos(strtolower(trim($NomIngredient)), $DebutRecherche) === 0)
+            array_push($Ingredients, $NomIngredient);
+    }
+
+    echo json_encode($Ingredients);
+
+?>
